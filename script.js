49ea0e994465c2820c201df4859573d21931654b
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-12 15:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,8 +18,15 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count = 0;
+	for (let i = 0; i < library.length; i++) {
+		if ('readingStatus' in i == true) {
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
 
-alert(numberOfBooksRead());
+// alert(numberOfBooksRead());
